97b58bcc4a4a3b5c80b9517fa9b59018415c3da4
JuanPabloCarrilloAcero <jcarrilloa@unal.edu.co>
2021-12-02 18:57:56 -0500

    Juan Carrillo
    28/11/2021
    
    Solucionado error buscar último elemento de Queue_linked_list.

--- main/src/Analizador.java
+++ main/src/Analizador.java
@@ -3,9 +3,10 @@ import java.util.*;
 public class Analizador {
 
     public static void main(String[] args){
-
+        /*
         Timer timer = new Timer();
         timer.schedule(new Emulador(), 0, 1000);
+         */
 
     }
 }
