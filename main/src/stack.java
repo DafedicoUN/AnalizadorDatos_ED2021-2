1e5e9dd87cf1232a9bae0a75492ac3476938d9db
Cristianchois <94801991+Cristianchois@users.noreply.github.com>
2021-12-05 20:56:57 -0500

    Add files via upload

---
+++ main/src/stack.java
@@ -0,0 +1,10 @@
+public interface stack {
+    void stacks(double data);
+    void unstack();
+    void stackFind(double find);
+    void stackDisplay();
+    void StackEmpty();
+    void Min();
+    void Max();
+    void StackAverage();
+}
