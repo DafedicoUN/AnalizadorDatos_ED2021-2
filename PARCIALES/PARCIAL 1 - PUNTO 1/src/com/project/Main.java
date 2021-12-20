244ccb0ec14f7d794ee3092403b4fc2a9ee00872
Juan Pablo Carrillo Acero <carrilloacerojuanpablo@gmail.com>
2021-12-20 13:22:19 -0500

    Creación de las clases bases para PUNTO 2

--- PARCIALES/PARCIAL 1 - PUNTO 1/src/com/project/Main.java
+++ PARCIALES/PARCIAL 1 - PUNTO 1/src/com/project/Main.java
@@ -1,5 +1,29 @@
 package com.project;
 
+interface List_interface {
+
+
+
+}
+
+class List implements List_interface {
+
+
+
+}
+
+class Queue extends List{
+
+
+
+}
+
+class Stack extends List{
+
+
+
+}
+
 public class Main {
 
     public static void main(String[] args) {
