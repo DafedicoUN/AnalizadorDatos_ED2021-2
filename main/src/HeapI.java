24802a3dd354652722b1a62d842057e94e94c0af
Juan Pablo Carrillo Acero <carrilloacerojuanpablo@gmail.com>
2022-02-05 15:48:41 -0500

    Terminado el ejemplo de heap

--- main/src/HeapI.java
+++ main/src/HeapI.java
@@ -1,45 +1,112 @@
+import heap.Heap;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 public class HeapI {
     public JPanel analizador;
     public JButton nuevoButton;
-    private JTable table1;
     private JTextField puntoTextField;
-    private JButton abrirButton;
-    private JButton guardarButton;
     private JButton conectarButton;
     private JButton iniciarButton;
     private JButton detenerButton;
-    //private Heap queue.queue = new queue.Queue_linked_list();
+    private JButton obtenerLas3MayoresButton;
+    private JButton promedioTemperaturaButton;
+    boolean connected = false;
+    private Heap h = new Heap();
+    boolean shutdown = false;
 
     HeapI(){
 
         conectarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
+
                 String textFieldValue = puntoTextField.getText();
+                if(!textFieldValue.equals("")){
+                    JOptionPane.showMessageDialog(null, "Conexión exitosa con el puerto " + textFieldValue);
+                    connected = true;
+                }
             }
         });
 
         iniciarButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-                //get data
-                //queue.queue.enqueue(data);
+                if(!connected){
+                    JOptionPane.showMessageDialog(null, "No se ha realizado una conexión");
+                } else {
+                    shutdown = false;
+                    emulador();
+                }
             }
         });
 
         nuevoButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent actionEvent) {
-
+                if(!connected){
+                    JOptionPane.showMessageDialog(null, "No se ha realizado una conexión");
+                } else {
+                    h = new Heap();
+                }
+            }
+        });
+        detenerButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                if(!connected){
+                    JOptionPane.showMessageDialog(null, "No se ha realizado una conexión");
+                } else {
+                    shutdown = true;
+                }
+            }
+        });
+        obtenerLas3MayoresButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                if(!connected){
+                    JOptionPane.showMessageDialog(null, "No se ha realizado una conexión");
+                } else {
+                    JOptionPane.showMessageDialog(null, h.max() + " Grados C°");
+                }
+            }
+        });
+        promedioTemperaturaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                if(!connected){
+                    JOptionPane.showMessageDialog(null, "No se ha realizado una conexión");
+                } else {
+                    JOptionPane.showMessageDialog(null, h.average() + " Grados C°");
+                }
             }
         });
     }
 
+    Runnable runnable = new Runnable() {
+        double i = 1;
+        public void run() {
+            if(!shutdown){
+                i++;
+                h.insert((int) Math.round(Math.log10(i / 2) * 81));
+                h.print();
+            } else {
+                i = 1;
+            }
+        }
+    };
+
+    public void emulador(){
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleAtFixedRate(runnable, 0, 3, TimeUnit.SECONDS);
+    }
+
     public static void main(String[] args){
-        System.out.println("HELLO");
+
     }
 }
--- main/src/heap/Heap.java
+++ main/src/heap/Heap.java
@@ -1,93 +1,76 @@
 package heap;
 
+import java.util.ArrayList;
+
 public class Heap {
+    ArrayList<Integer> a = new ArrayList<Integer>();
 
-    private int[] Heap;
-    private int size;
-    private int maxsize;
+    void heapify(int i) {
+        int size = a.size();
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
+        if (l < size && a.get(l) > a.get(largest))
+            largest = l;
+        if (r < size && a.get(r) > a.get(largest))
+            largest = r;
 
-    //Inicializa un heap con capacidad máxima
-    public Heap(int maxsize) {
-        this.maxsize = maxsize;
-        this.size = 0;
-        Heap = new int[this.maxsize];
+        if (largest != i) {
+            int temp = a.get(largest);
+            a.set(largest, a.get(i));
+            a.set(i, temp);
+
+            heapify(largest);
+        }
     }
 
-    //Posición padre
-    private int parent(int pos) {
-        return (pos - 1) / 2;
-    }
-
-    //Posición hijo izquierda
-    private int leftChild(int pos) {
-        return (2 * pos);
-    }
-
-    //Posición hijo derecha
-    private int rightChild(int pos) {
-        return (2 * pos) + 1;
-    }
-
-    //Verifica si el nodo es hijo
-    private boolean isLeaf(int pos) {
-        return pos > (size / 2) && pos <= size;
-    }
-
-    //Intercambia nodos
-    private void swap(int fpos, int spos) {
-        int tmp = Heap[fpos];
-        Heap[fpos] = Heap[spos];
-        Heap[spos] = tmp;
-    }
-
-    //Función recursiva para max heapify dado un subtree
-    private void maxHeapify(int pos) {
-        if (isLeaf(pos))
-            return;
-
-        if (Heap[pos] < Heap[leftChild(pos)]
-                || Heap[pos] < Heap[rightChild(pos)]) {
-
-            if (Heap[leftChild(pos)]
-                    > Heap[rightChild(pos)]) {
-                swap(pos, leftChild(pos));
-                maxHeapify(leftChild(pos));
-            } else {
-                swap(pos, rightChild(pos));
-                maxHeapify(rightChild(pos));
+    public void insert(int newNum) {
+        int size = a.size();
+        a.add(newNum);
+        if (size != 0) {
+            for (int i = size / 2 - 1; i >= 0; i--) {
+                heapify(i);
             }
         }
     }
 
-    //Insertar elemento
-    public void insert(int element) {
-        Heap[size] = element;
-
-        //Verifica que se cumpla la propiedad del heap
-        int current = size;
-        while (Heap[current] > Heap[parent(current)]) {
-            swap(current, parent(current));
-            current = parent(current);
+    void delete(int num)
+    {
+        int size = a.size();
+        int i;
+        for (i = 0; i < size; i++)
+        {
+            if (num == a.get(i))
+                break;
         }
-        size++;
+
+        int temp = a.get(i);
+        a.set(i, a.get(size-1));
+        a.set(size-1, temp);
+
+        a.remove(size-1);
+        for (int j = size / 2 - 1; j >= 0; j--)
+        {
+            heapify(j);
+        }
     }
 
-    //Imprime el heap
     public void print() {
-
-        for (int i = 0; i < size / 2; i++) {
-
-            System.out.print("Parent Node : " + Heap[i]);
-
-            if (leftChild(i) < size) //if the child is out of the bound of the array
-                System.out.print(" Left Child Node: " + Heap[leftChild(i)]);
-
-            if (rightChild(i) < size) //if the right child index must not be out of the index of the array
-                System.out.print(" Right Child Node: " + Heap[rightChild(i)]);
-
-            System.out.println(); //for new line
-
+        for (Integer i : a) {
+            System.out.print(i + " ");
         }
+        System.out.println();
+    }
 
+    public int max() {
+        return a.get(0);
+    }
+
+    public int average(){
+        int sum = 0;
+        for (Integer i : a) {
+            sum += i;
+        }
+        return sum / a.size();
     }
 }
